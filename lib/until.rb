97318486a43b56024f57f2535f97808691547924
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-12 04:00:32 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,8 +1,9 @@
 def using_until
   levitation_force = 6
   #your code here
-  untile levitation_force == 10
+  until levitation_force == 10
     puts "Wingardium Leviosa"
     levitation_force++
+  end
 end
 
